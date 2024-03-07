caddc2c108c0edd2bb114f1afe59b6d03189fa7c
cthdarren <cthdarren88@gmail.com>
2024-03-07 21:14:32 +0800

    removed most scaffolded code, trying to create a running api

--- src/main/java/com/wayfare/backend/repository/ItemRepository.java
+++ src/main/java/com/wayfare/backend/repository/ItemRepository.java
@@ -3,17 +3,12 @@ package com.wayfare.backend.repository;
 import com.wayfare.backend.model.GroceryItem;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
+@RepositoryRestResource(collectionResourceRel = "groceries", path = "groceries")
 
 public interface ItemRepository extends MongoRepository<GroceryItem, String> {
-
-    @Query("{name:'?0'}")
-    GroceryItem findItemByName(String name);
-
-    @Query(value="{category:'?0'}", fields="{'name' : 1, 'quantity' : 1}")
-    List<GroceryItem> findAll(String category);
-
-    public long count();
-
+    List<GroceryItem> findByName(@Param("name") String name);
 }
