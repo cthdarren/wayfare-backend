59588f6c09f6f1d2675341a185b66e4136dffefc
cthdarren <cthdarren88@gmail.com>
2024-03-07 19:19:23 +0800

    initial

---
+++ src/main/java/com/example/wayfarebackend/WayfareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.wayfarebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WayfareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WayfareBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wayfarebackend/WayfareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wayfarebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WayfareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
