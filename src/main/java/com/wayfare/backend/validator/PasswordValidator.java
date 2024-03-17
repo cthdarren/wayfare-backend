86e8adb71d041a82d1530a2f10b45d0ad375f3c9
cthdarren <cthdarren88@gmail.com>
2024-03-17 23:08:25 +0800

    changed password to max 30 chars

--- src/main/java/com/wayfare/backend/validator/PasswordValidator.java
+++ src/main/java/com/wayfare/backend/validator/PasswordValidator.java
@@ -8,13 +8,13 @@ import java.util.regex.Pattern;
         password must contain 1 uppercase letters
         password must contain 1 lowercase letters
         password must contain 1 non-alpha numeric number
-        password is 8-16 characters with no space
+        password is 8-30 characters with no space
 */
 
 
 public class PasswordValidator extends BaseValidator {
 
-    public static Pattern pattern = Pattern.compile("^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\\w\\d\\s:])([^\\s]){8,}$");
+    public static Pattern pattern = Pattern.compile("^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\\w\\d\\s:])([^\\s]){8,30}$");
     public static String errMsg = "Invalid password format";
     public PasswordValidator(String input){
         super(pattern, errMsg, input);
