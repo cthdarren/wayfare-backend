3ec5e3c10715f5b40b4b4578ee2d742536af28a5
cthdarren <cthdarren88@gmail.com>
2024-03-21 21:02:22 +0800

    added nullable to findbyurl

--- src/main/java/com/wayfare/backend/repository/VerifyURLRepository.java
+++ src/main/java/com/wayfare/backend/repository/VerifyURLRepository.java
@@ -1,8 +1,10 @@
 package com.wayfare.backend.repository;
 
+import com.mongodb.lang.Nullable;
 import com.wayfare.backend.model.VerifyURL;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface VerifyURLRepository extends MongoRepository<VerifyURL, String> {
+    @Nullable
     VerifyURL findByUrl(String url);
 }
