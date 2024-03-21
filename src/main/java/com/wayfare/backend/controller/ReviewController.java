782779027d0ba22d38bc11fabf1d96fd1e43ce0e
cthdarren <cthdarren88@gmail.com>
2024-03-21 21:35:49 +0800

    changed urls to standardise for public vs non public urls

--- src/main/java/com/wayfare/backend/controller/ReviewController.java
+++ src/main/java/com/wayfare/backend/controller/ReviewController.java
@@ -4,19 +4,20 @@ import com.wayfare.backend.model.Review;
 import com.wayfare.backend.repository.ReviewRepository;
 import com.wayfare.backend.repository.UserRepository;
 import com.wayfare.backend.request.CreateReviewRequest;
+import com.wayfare.backend.request.ReviewsByListingRequest;
 import com.wayfare.backend.response.ResponseObject;
 import com.wayfare.backend.security.WayfareUserDetails;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.Optional;
 
 import static com.wayfare.backend.helper.helper.getCurrentUserDetails;
 
 @RestController
-@RequestMapping("/api/review")
 public class ReviewController {
     private final ReviewRepository reviewRepo;
 
@@ -27,29 +28,32 @@ public class ReviewController {
         this.userRepo = userRepo;
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/api/review/{id}")
     public ResponseObject getReview(@PathVariable String id) {
         Optional<Review> review = reviewRepo.findById(id);
         if (review.isEmpty()) {
-            return new ResponseObject(false, "review not found!")
+            return new ResponseObject(false, "Review not found");
         } else {
             return new ResponseObject(true, review);
         }
     }
 
-    @GetMapping("/listing/{listingId}")
-    public ResponseObject getReviewsByListing(@PathVariable String listingId){
-        List<Review> reviewList = reviewRepo.findAllByListingId(listingId);
+    @PostMapping("/api/listing/reviews")
+    public ResponseObject getReviewsByListing(@RequestBody ReviewsByListingRequest request){
+        List<Review> reviewList = reviewRepo.findAllByListingId(request.listingId());
+
         return new ResponseObject(true, reviewList);
     }
 
-    @GetMapping("/")
+    // MUST BE AUTHORISED AS USER
+    @GetMapping("/reviews")
     public ResponseObject getReviewsByUser(){
         WayfareUserDetails user = getCurrentUserDetails();
         List<Review> reviewList = reviewRepo.findAllByUserId(user.getId());
         return new ResponseObject(true, reviewList);
     }
 
+    // MUST BE AUTHORISED AS USER
     @PostMapping("/create")
     public ResponseObject createReview(@RequestBody CreateReviewRequest request){
         WayfareUserDetails user = getCurrentUserDetails();
@@ -61,7 +65,7 @@ public class ReviewController {
                 request.listingId()
         );
         reviewRepo.save(toAdd);
-        return new ResponseObject(true, "review added");
+        return new ResponseObject(true, "Review added");
     }
 }
 
---
+++ src/main/java/com/wayfare/backend/request/ReviewsByListingRequest.java
@@ -0,0 +1,4 @@
+package com.wayfare.backend.request;
+
+public record ReviewsByListingRequest(String listingId) {
+}
