33f9d66cd234c974004c775176a75b407a08f4f0
cthdarren <cthdarren88@gmail.com>
2024-03-26 00:02:12 +0800

    get users listings by username

--- src/main/java/com/wayfare/backend/controller/TourController.java
+++ src/main/java/com/wayfare/backend/controller/TourController.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.wayfare.backend.model.User;
 import com.wayfare.backend.model.dto.TourListingDTO;
+import com.wayfare.backend.repository.UserRepository;
 import com.wayfare.backend.request.LocationRequest;
 import com.wayfare.backend.request.TourListingsByUser;
 import com.wayfare.backend.response.ResponseObject;
@@ -23,10 +25,12 @@ import static com.wayfare.backend.helper.helper.getCurrentUserDetails;
 
 @RestController
 public class TourController {
+    private final UserRepository userRepo;
     private final TourRepository tourRepo;
 
     ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
-    public TourController(TourRepository tourRepo) {
+    public TourController(UserRepository userRepo, TourRepository tourRepo) {
+        this.userRepo = userRepo;
         this.tourRepo = tourRepo;
     }
 
@@ -47,9 +51,13 @@ public class TourController {
         return new ResponseObject(true, listByCountry);
     }
 
-    @GetMapping("/api/v1/listing/user")
-    public ResponseObject getListingsByUserId(@RequestBody TourListingsByUser request) {
-        List<TourListing> listByUserId = tourRepo.findAllByUserId(request.userId());
+    @GetMapping("/api/v1/user/listing/{username}")
+    public ResponseObject getListingsByUserId(@PathVariable String username) {
+        User user = userRepo.findByUsername(username);
+        if (user == null){
+            return new ResponseObject(false, "Username not found");
+        }
+        List<TourListing> listByUserId = tourRepo.findAllByUserId(user.getId());
         return new ResponseObject(true, listByUserId);
     }
 
