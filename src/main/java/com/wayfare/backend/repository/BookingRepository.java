6fac741bcfe2ad9ceeac4eceb6199c9452fae3bf
haresh02 <haresh.jayant@gmail.com>
2024-03-28 14:40:35 +0800

    bookingdto and bookingrepo filter in progress

--- src/main/java/com/wayfare/backend/repository/BookingRepository.java
+++ src/main/java/com/wayfare/backend/repository/BookingRepository.java
@@ -1,10 +1,12 @@
 package com.wayfare.backend.repository;
 
 import com.wayfare.backend.model.Booking;
+import com.wayfare.backend.model.object.TimeRange;
 
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Date;
 import java.util.List;
 
 public interface BookingRepository extends MongoRepository<Booking, String> {
@@ -14,6 +16,11 @@ public interface BookingRepository extends MongoRepository<Booking, String> {
     List<Booking> findAllByUserId(String userId);
     List<Booking> findAllByWayFarerId(String wayFarerId);
 
-    // aggregation pipeline date booked --> time slot
-
+    // aggregation pipeline
+    // match by datedBooked first
+    // match by bookingDuration start time and end time
+    @Aggregation(pipeline = {
+            "{ $match : { dateBooked : ?0, bookingDuration : ?1}}",
+    })
+    List<Booking> findByDateAndTime(Date dateBooked, TimeRange bookingDuration);
 }
